ddd0011df3b6bfb215d9a5fc9a1b2c42f2b2018d
Yolanda <yolandacorderorobledo@gmail.com>
2023-03-14 20:44:41 +0100

    variables y template string

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+// Variables y constantes
+const nombre = 'Yolanda';
+const apellido = 'Cordero';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true){
+    let valorDado = 6;
+    const nombre = 'Pepe';
+    console.log(valorDado);
+}
+
+console.log(valorDado);
---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,14 @@
+const nombre = 'Yolanda';
+const apellido = 'Cordero';
+
+//const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto=`${ nombre } ${ apellido }`;
+
+
+console.log( nombreCompleto );
+
+function getSaludo(nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${ getSaludo(nombre)}` );
